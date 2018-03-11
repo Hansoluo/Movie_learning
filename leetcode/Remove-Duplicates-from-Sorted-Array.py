7e53adea8c89a299f4dc69f2f74e1cd475bd975f
Hansoluo <shy757@126.com>
2018-03-11 09:11:28 +0800

    test error:Time Limit Exceeded

---
+++ leetcode/Remove-Duplicates-from-Sorted-Array.py
@@ -0,0 +1,30 @@
+# coding: utf-8
+
+# problem:
+# Given a sorted array, remove the duplicates in-place such that each element appear only once and return the new length.
+# Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+class Solution:
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int}
+        :rtype: int
+        """
+        a = len(nums)
+        i = 0
+        while i < a:
+            b = nums[i]
+            nums.pop(i)
+            if b not in nums:
+            	nums.insert(i,b)
+            	i = i + 1
+            else:
+            	a = len(nums)
+        return len(nums)
+
+nums = [0,4,4,0,4,4,4,0,2]
+s = Solution()
+print(s.removeDuplicates(nums))
+
+
+
